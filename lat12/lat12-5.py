2aa13ba3ac02610da85a822fdafb811ad93e4625
rayhanzfr <rayhanzaf@gmail.com>
2021-03-15 17:00:29 +0700

    Update lat12-5.py

--- lat12/lat12-5.py
+++ lat12/lat12-5.py
@@ -1,12 +1,13 @@
 def int_or_not():
     userinput=(input("Masukkan Inputan: "))
-    if userinput.isdigit()==True:
-        print("INTEGER")
-        return " "
-    elif userinput.isalnum():
-        print("BUKAN INTEGER")
-        return " "
+    if userinput.isalnum():
+        if userinput.isdigit():
+            print("INTEGER")
+            return " "
+        else:
+            print("BUKAN INTEGER")
+            return " "
     else:
-        print("Masukkan yang bener")
+        print("Masukkan input yang benar")
         return int_or_not()
 print(int_or_not())
